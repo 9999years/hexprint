9cba25af29c62d45c6fd074f0d2d029caf8fc898
Rebecca Turner <637275@gmail.com>
2017-07-27 00:15:23 -0400

    setup

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='hexprint',
-    version='0.0.1',
+    version='0.0.2',
 
-    description='''A utility for printing raw bytes to the console or converting a string like `1b 20 f9` or `1B20F9` into the bytes b'\x1b\x20\xf9'.''',
+    description=r'''A utility for printing raw bytes to the console or converting a string like `1b 20 f9` or `1B20F9` into the bytes b'\x1b\x20\xf9'.''',
     long_description='https://github.com/9999years/hexprint',
     url='https://github.com/9999years/hexprint',
     author='Rebecca Turner',
